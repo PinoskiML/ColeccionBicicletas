24730239d83317c99aed9c7adb83fdd188856667
PinoskiML <felipe.montagne.lascaris@gmail.com>
2025-07-03 22:09:27 +0200

    clean

--- Main.java
+++ Main.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 public class Main {
 
 
-
     public static void main(String[] args) {
 
         System.out.println("Colección de bicicletas");
-        
+
         ArrayList<Bicicleta> bikeList = Bicicleta.newBikeArray();
 
-       // System.out.println(bikeList.get()); //debug shows newBikeArray is made
+        // System.out.println(bikeList.get()); //debug shows newBikeArray is made
 
         Bicicleta.addBikeWithSetParemeters(bikeList);
 
@@ -26,10 +25,10 @@ public class Main {
 
         System.out.println(bikeList); //updated list
         System.out.println("\n- Set make of the first bike to: We The People");
-        System.out.println("Original bike: " +bikeList.get(0));
+        System.out.println("Original bike: " + bikeList.get(0));
         bikeList.get(0).setMake("We The People");
 
-        System.out.println("Updated entry: " +bikeList.get(0));
+        System.out.println("Updated entry: " + bikeList.get(0));
 
 
         System.out.println("\n - Use some random methods :)  \n");
@@ -39,12 +38,8 @@ public class Main {
         System.out.println("ZZZzzzzZZzzzzz");
 
 
-
-
-
         //end of main
-        }
-
+    }
 
 
 }
